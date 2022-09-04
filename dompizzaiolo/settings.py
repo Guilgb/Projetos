3153e64c41b36561005f944313f9540b1b1dc0e0
guilherme <guilherme.gb@hotmail.com>
2022-09-04 13:46:50 -0300

    deploy off

--- dompizzaiolo/settings.py
+++ dompizzaiolo/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,4 +112,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
